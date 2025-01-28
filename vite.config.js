5121432fc81367f8694f50e89f7ec37879a43c30
Tarek Roukos <t.r@lemonadefashion.com>
2025-01-28 05:51:47 +0200

    bla2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',  // Set the base path for GitHub Pages
+  base: '/dummyPhone/',  // Set the base path for GitHub Pages
   plugins: [
     react(),
     sentryVitePlugin({
